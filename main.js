11d9f6cd9d368b73a80c49911af0936bdb45e51d
Kevin Han <khan@colgate.edu>
2022-03-22 16:47:13 -0400

    date and to do title

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const{app, BrowserWindow} = require('electron')
-
 const createWindow = () =>{
     const win = new BrowserWindow({
         show: false
@@ -7,10 +6,7 @@ const createWindow = () =>{
     win.maximize();
     win.show();
     win.loadFile('index.html')
-<<<<<<< HEAD
-=======
 
->>>>>>> 0707951eafe9b1ea8cd3d876a6e9105fa58f693a
 }
 // Create the window when app is ready
 app.whenReady().then(() =>{
@@ -20,21 +16,44 @@ app.whenReady().then(() =>{
     })
 })
 
-<<<<<<< HEAD
-app.on('window-all-closed', () =>{
-    if (process.platform !== 'darwin') app.quit()
-})
-=======
 // deal with app closure
 app.on('window-all-closed', () =>{
     if (process.platform !== 'darwin') app.quit()
 })
-// Controls videoplayback if focused / unfocused
-window.onfocus = () => {
-    document.querySelector('.videobg').play();
-}
-window.onblur = () => {
-    document.querySelector('.videobg').pause();
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = ((h + 11) % 12 + 1);
+    let suffix = h <= 12 ? "PM" : "AM"
+
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML = h + ":" + m + ":" + s + " " + suffix;
+    setTimeout(startTime, 1000);
+    setTimeout(startDate, 1000);
 }
 
->>>>>>> 0707951eafe9b1ea8cd3d876a6e9105fa58f693a
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
+
+function startDate(){
+    const today = new Date();
+    const daysOfWeek = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+    let d = today.getDate();
+    let m = today.getMonth();
+    let y = today.getFullYear();
+    let weekday = today.getDay();
+
+    const date = document.getElementById('dateformat');
+    const day = document.getElementById('day');
+    date.innerHTML = `${m+1}/${d}/${y}`;
+    day.innerHTML = daysOfWeek[weekday-1];
+
+}
+
